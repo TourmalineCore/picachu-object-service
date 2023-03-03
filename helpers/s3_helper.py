1e6f93e31b5fe28621f49a598d38e34bf3042c9a
Maria Yadryshnikova <93837471+myadryshnikova@users.noreply.github.com>
2023-03-03 14:19:08 +0500

    fix(path): fix path to the s3_config

--- helpers/s3_helper.py
+++ helpers/s3_helper.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING, Optional
 
 import boto3
 
-import config
+import config.s3_config
 from helpers.s3_paths import append_prefix
 
 ACL_PRIVATE = 'private'
@@ -30,23 +30,23 @@ class Singleton(type):
 class S3Helper(metaclass=Singleton):
     def __init__(self):
         self.session = boto3.session.Session(
-            aws_access_key_id=config.s3_access_key_id,
-            aws_secret_access_key=config.s3_secret_access_key,
+            aws_access_key_id=config.s3_config.s3_access_key_id,
+            aws_secret_access_key=config.s3_config.s3_secret_access_key,
         )
 
     def get_resource(self) -> S3ServiceResource:
         resource: S3ServiceResource = self.session.resource(
             's3',
-            endpoint_url=config.s3_endpoint,
-            use_ssl=config.s3_use_ssl,
+            endpoint_url=config.s3_config.s3_endpoint,
+            use_ssl=config.s3_config.s3_use_ssl,
         )
         return resource
 
     def s3_download_file(
             self,
             file_path_in_bucket,
-            s3_bucket_name: str = config.s3_bucket_name,
-            s3_prefix: Optional[str] = config.s3_prefix,
+            s3_bucket_name: str = config.s3_config.s3_bucket_name,
+            s3_prefix: Optional[str] = config.s3_config.s3_prefix,
     ) -> bytes:
         bucket: Bucket = self.get_resource().Bucket(s3_bucket_name)
         result_path_in_bucket = append_prefix(path=file_path_in_bucket, prefix=s3_prefix)
